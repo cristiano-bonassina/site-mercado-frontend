ab32f5411ed3bfc9afe9c53322222ba495bb7b6a
Cristiano Bonassina <cristiano@logicart.com.br>
2021-03-08 15:16:28 -0300

    Adicionando mensagens de erro no cadastro de produto

--- src/app/features/products/components/product-detail/product-detail.component.ts
+++ src/app/features/products/components/product-detail/product-detail.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { Subject } from "rxjs";
 import { Product } from "../../models/product";
@@ -8,6 +9,7 @@ import { ProductService } from "../../services/product.service";
   templateUrl: "./product-detail.component.html",
 })
 export class ProductDetailComponent {
+  public readonly errorMesssages = new Array<string>();
   public isSavingChanges?: boolean;
   public product?: Product;
   public readonly registrationCompleted = new Subject<Product>();
@@ -15,6 +17,8 @@ export class ProductDetailComponent {
   public constructor(private readonly productService: ProductService) {}
 
   public async saveProductChanges() {
+    this.errorMesssages.length = 0;
+
     if (!this.product) {
       return;
     }
@@ -25,7 +29,14 @@ export class ProductDetailComponent {
       await this.productService.saveProduct(this.product);
       this.registrationCompleted.next(this.product);
     } catch (error) {
-      console.log({ error });
+      if (error instanceof HttpErrorResponse && error.error.errors) {
+        const errorMesssages: string[] = error.error.errors;
+        this.errorMesssages.push(...errorMesssages);
+        return;
+      }
+
+      const errorMessage = "productDetailComponent.errorSavingProduct";
+      this.errorMesssages.push(errorMessage);
     } finally {
       this.isSavingChanges = false;
     }
